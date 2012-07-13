b0dce6e9626d7de1df40a779ff555dcbc403f054
thankkub <pliastudio@gmail.com>
2012-07-13 22:32:15 +0700

    Commit 01

---
+++ WingKwai_Game/src/wingkwai/main/Game1.java
@@ -0,0 +1,35 @@
+package wingkwai.main;
+
+import plia.core.Game;
+import plia.core.scene.Group;
+import plia.core.scene.Layer;
+import plia.core.scene.Scene;
+import plia.core.scene.View;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Game1 extends Game
+{
+	private Scene scene = new Scene();
+	private Layer<Group> layer1 = new Layer<Group>();
+	private Layer<View> layer2 = new Layer<View>();
+	
+	@Override
+	public void onInitialize(Bundle arg0)
+	{
+		setRequestedOrientation(0);
+		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		setScene(scene);
+	}
+	@Override
+	public void onUpdate()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
